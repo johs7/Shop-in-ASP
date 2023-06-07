d68f8572d4c169ffb3cdd9a8b901c27e956223de
Johan <Roquejohanssen@gmail.com>
2023-06-07 17:29:54 -0600

    ACTUALIZANDO CAMBIOS.BOOSTRAP 5 MENU INICIAL

--- Lorinos/App_Start/BundleConfig.cs
+++ Lorinos/App_Start/BundleConfig.cs
@@ -22,9 +22,7 @@ namespace Lorinos
             bundles.Add(new Bundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.bundle.js"));
             
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include ("~/Content/site.css"));
         }
     }
 }
