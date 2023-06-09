5d765e3433767e7d9181a0790d7928daa3c2f610
Johan <Roquejohanssen@gmail.com>
2023-06-09 00:33:56 -0600

    atributos usuario

--- CapaEntidad/ClassUsuario.cs
+++ CapaEntidad/ClassUsuario.cs
@@ -8,5 +8,12 @@ namespace CapaEntidad
 {
     public class ClassUsuario
     {
+        public int IdUsuario { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set;}
+        public  string Correo { get; set; }
+        public string Clave { get; set; }
+        public  bool Reestablecer { get; set; }
+        public bool Activo { get; set; }
     }
 }
