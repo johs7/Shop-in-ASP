62498654c4be6445502e094a51ed33d046c245f7
Johan <Roquejohanssen@gmail.com>
2023-06-14 16:24:52 -0600

    registro en sql

--- CapaDatos/ClassCDUsuarios.cs
+++ CapaDatos/ClassCDUsuarios.cs
@@ -48,5 +48,38 @@ namespace CapaDatos
             }
             return Lista;
         }
+
+        public int Registrar(ClassUsuario obj,out string Mensaje)
+        {
+            int idautogenerado = 0;
+            Mensaje = string.Empty;
+            try
+            {
+                using(SqlConnection oconexion=new SqlConnection(ClassConexion.cn))
+                {
+                    SqlCommand cmd=new SqlCommand("sp_RegistrarUsuario", oconexion);
+                    cmd.Parameters.AddWithValue("Nombres", obj.Nombres);
+                    cmd.Parameters.AddWithValue("Apellidos", obj.Apellidos);
+                    cmd.Parameters.AddWithValue("Correo", obj.Correo);
+                    cmd.Parameters.AddWithValue("Clave", obj.Clave);
+                    cmd.Parameters.AddWithValue("Activo", obj.Activo);
+                    cmd.Parameters.Add("Resultado",SqlDbType.Int).Direction=ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje",SqlDbType.VarChar,500).Direction=ParameterDirection.Output;
+                    cmd.CommandType=CommandType.StoredProcedure;
+                    oconexion.Open();
+                    cmd.ExecuteNonQuery();
+                    idautogenerado = Convert.ToInt32(cmd.Parameters["Resultado"].Value.ToString());
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+
+                }
+
+            }
+            catch(Exception ex)
+            {
+                idautogenerado = 0;
+                Mensaje = ex.Message;
+            }
+            return idautogenerado;
+        }
     }
 }
