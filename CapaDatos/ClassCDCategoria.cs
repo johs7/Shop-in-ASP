2b16be532ec8d5677fc4300ef5e77d7d95cef489
Johan <Roquejohanssen@gmail.com>
2023-06-23 15:26:13 -0600

    registrar categoria cd

--- CapaDatos/ClassCDCategoria.cs
+++ CapaDatos/ClassCDCategoria.cs
@@ -44,5 +44,34 @@ namespace CapaDatos
             }
             return Lista;
         }
+        public int Registrar(ClassCategoria obj, out string Mensaje)
+        {
+            int idautogenerado = 0;
+            Mensaje = string.Empty;
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(ClassConexion.cn))
+                {
+                    SqlCommand cmd = new SqlCommand("sp_RegistrarCategoria", oconexion);
+                    cmd.Parameters.AddWithValue("Descripcion", obj.Descripcion);
+                    cmd.Parameters.AddWithValue("Categoria", obj.Activo);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    oconexion.Open();
+                    cmd.ExecuteNonQuery();
+                    idautogenerado = Convert.ToInt32(cmd.Parameters["Resultado"].Value);
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                idautogenerado = 0;
+                Mensaje = ex.Message;
+            }
+            return idautogenerado;
+        }
     }
 }
