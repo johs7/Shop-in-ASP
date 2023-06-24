4474e313642f3ebb442457fb5a8afc7144a5467e
Johan <Roquejohanssen@gmail.com>
2023-06-24 10:57:42 -0600

    clase cn categoria

--- CapaNegocios/ClassCNCategoria.cs
+++ CapaNegocios/ClassCNCategoria.cs
@@ -3,10 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CapaDatos;
+using CapaEntidad;
 
 namespace CapaNegocios
 {
    public class ClassCNCategoria
     {
+        private ClassCDCategoria objCapaDato = new ClassCDCategoria();
+        public List<ClassCategoria> Listar()
+        {
+            return objCapaDato.Listar();
+        }
+        public int Registrar( ClassCategoria obj, out string Mensaje)
+        {
+            Mensaje = string.Empty;
+
+            if (string.IsNullOrEmpty(obj.Descripcion) || string.IsNullOrWhiteSpace(obj.Descripcion))
+            {
+                Mensaje = "El campo Descripción es obligatorio";
+            }
+            if (string.IsNullOrEmpty(Mensaje))
+            {
+                return objCapaDato.Registrar(obj, out Mensaje);
+            }
+            else
+            {
+                return 0;
+            }
+          
+        }
+        public bool Editar(ClassCategoria obj, out string Mensaje)
+        {
+            Mensaje = string.Empty;
+
+            if (string.IsNullOrEmpty(obj.Descripcion) || string.IsNullOrWhiteSpace(obj.Descripcion))
+            {
+                Mensaje = "El campo Nombres es obligatorio";
+            }
+            if (string.IsNullOrEmpty(Mensaje))
+            {
+                return objCapaDato.Editar(obj, out Mensaje);
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        public bool Eliminar(int id, out string Mensaje)
+        {
+            return objCapaDato.Eliminar(id, out Mensaje);
+        }
     }
 }
