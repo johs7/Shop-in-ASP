2a8ff0f7550bc981b1945b554271b5edb014d848
Johan <Roquejohanssen@gmail.com>
2023-07-27 13:58:27 -0600

    AGREGANDO MARCA

---
+++ CapaNegocios/ClassCN_Marca.cs
@@ -0,0 +1,59 @@
+﻿using CapaDatos;
+using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocios
+{
+    public class ClassCN_Marca
+    {
+        private ClassCDMarca objCapaDato = new ClassCDMarca();
+        public List<ClassMarca> Listar()
+        {
+            return objCapaDato.Listar();
+        }
+        public int Registrar(ClassMarca obj, out string Mensaje)
+        {
+            Mensaje = string.Empty;
+
+            if (string.IsNullOrEmpty(obj.Descripcion) || string.IsNullOrWhiteSpace(obj.Descripcion))
+            {
+                Mensaje = "El campo Descripción es obligatorio";
+            }
+            if (string.IsNullOrEmpty(Mensaje))
+            {
+                return objCapaDato.Registrar(obj, out Mensaje);
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+        public bool Editar(ClassMarca obj, out string Mensaje)
+        {
+            Mensaje = string.Empty;
+
+            if (string.IsNullOrEmpty(obj.Descripcion) || string.IsNullOrWhiteSpace(obj.Descripcion))
+            {
+                Mensaje = "El campo Nombres es obligatorio";
+            }
+            if (string.IsNullOrEmpty(Mensaje))
+            {
+                return objCapaDato.Editar(obj, out Mensaje);
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        public bool Eliminar(int id, out string Mensaje)
+        {
+            return objCapaDato.Eliminar(id, out Mensaje);
+        }
+    }
+}
--- Lorinos/Controllers/MantenedorController.cs
+++ Lorinos/Controllers/MantenedorController.cs
@@ -5,8 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using CapaEntidad;
-using CapaNegocios;
+
     namespace Lorinos.Controllers
 {
     public class MantenedorController : Controller
@@ -24,6 +23,7 @@ using CapaNegocios;
         {
             return View();
         }
+        #region categoria
         [HttpGet]
         public JsonResult ListarCategorias()
         {
@@ -56,5 +56,42 @@ using CapaNegocios;
             respuesta = new ClassCNCategoria().Eliminar(id, out mensaje);
             return Json(new { resultado = respuesta, mensaje = mensaje }, JsonRequestBehavior.AllowGet);
         }
+        #endregion
+
+        #region marca
+        [HttpGet]
+        public JsonResult ListarMarca()
+        {
+            List<ClassMarca> oLista = new List<ClassMarca>();
+            oLista = new ClassCN_Marca().Listar();
+            return Json(new { data = oLista }, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public JsonResult GuardarMarca(ClassMarca obj)
+        {
+            object resultado;
+            string mensaje = string.Empty;
+
+            if (obj.IdMarca == 0)
+            {
+                resultado = new ClassCN_Marca().Registrar(obj, out mensaje);
+            }
+            else
+            {
+                resultado = new ClassCN_Marca().Editar(obj, out mensaje);
+            }
+            return Json(new { resultado = resultado, mensaje = mensaje }, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public JsonResult EliminarMarca(int id)
+        {
+            bool respuesta = false;
+            string mensaje = string.Empty;
+
+            respuesta = new ClassCN_Marca().Eliminar(id, out mensaje);
+            return Json(new { resultado = respuesta, mensaje = mensaje }, JsonRequestBehavior.AllowGet);
+        }
+        #endregion
+
     }
 }
